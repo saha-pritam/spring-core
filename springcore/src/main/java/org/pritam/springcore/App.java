fb12c1298ac2d25744fcf89b36b59ae214f021a9
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 18:49:32 +0530

    Topic 11

--- springcore/src/main/java/org/pritam/springcore/App.java
+++ springcore/src/main/java/org/pritam/springcore/App.java
@@ -9,8 +9,6 @@ public class App
     public static void main( String[] args )
     {
         AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("org/pritam/springcore/config.xml");
-        Student s = (Student) applicationContext.getBean("student");
-        System.out.println(s);
         applicationContext.close();
     }
 }
