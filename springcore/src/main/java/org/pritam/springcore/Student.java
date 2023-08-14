baaad851749039cb5c3ed705be75ca566d223207
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 17:05:15 +0530

    Topic 10-b

--- springcore/src/main/java/org/pritam/springcore/Student.java
+++ springcore/src/main/java/org/pritam/springcore/Student.java
@@ -2,6 +2,7 @@ package org.pritam.springcore;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,10 +10,9 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 public class Student {
 	private int id;
 	private String firstName;
 	private String lastName;
-	private Address address;
-
 }
