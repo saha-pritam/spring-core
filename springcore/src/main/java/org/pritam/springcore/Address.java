090cb6034909fa1176239d0407985e6d2c69313a
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 16:52:45 +0530

    Topic 9. I forgot to add the Address.java class from topic 9-a to 9-c. Make sure to add it then execute from 9-a to 9-c othwise you will face error.

---
+++ springcore/src/main/java/org/pritam/springcore/Address.java
@@ -0,0 +1,13 @@
+package org.pritam.springcore;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Address {
+	private String city;
+	private String country;
+}
