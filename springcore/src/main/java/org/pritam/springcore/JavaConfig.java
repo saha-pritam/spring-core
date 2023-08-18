d46fff8118c14cf3ec55839f3e6129a5334d63cc
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-18 20:26:21 +0530

    Topic-23-d

--- springcore/src/main/java/org/pritam/springcore/JavaConfig.java
+++ springcore/src/main/java/org/pritam/springcore/JavaConfig.java
@@ -1,10 +1,12 @@
 package org.pritam.springcore;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "org.pritam.springcore")
 public class JavaConfig {
-
+	@Bean
+	public Model getModel() {
+		return new Model();
+	}
 }
